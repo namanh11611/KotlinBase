486d41cf7c8bf8727f93c4ae064e34b54d006c36
Nam Anh Nguyen <namanh11611@gmail.com>
2022-06-19 10:51:29 +0700

    Update libs version

--- app/src/main/java/com/namanh/kotlinbase/data/database/NewsDao.kt
+++ app/src/main/java/com/namanh/kotlinbase/data/database/NewsDao.kt
@@ -10,20 +10,20 @@ import com.namanh.kotlinbase.data.model.News
 @Dao
 interface NewsDao {
     @Query("SELECT * FROM news")
-    suspend fun getAll(): List<News>
+    fun getAll(): List<News>
 
     @Query("SELECT * FROM news WHERE id LIKE :newsId")
-    suspend fun getNewsById(newsId: Int): News
+    fun getNewsById(newsId: Int): News
 
     @Query("SELECT * FROM news WHERE title LIKE :title LIMIT 1")
-    suspend fun findByTitle(title: String): News
+    fun findByTitle(title: String): News
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(news: List<News>)
+    fun insertAll(news: List<News>)
 
     @Query("DELETE FROM news")
-    suspend fun deleteAll()
+    fun deleteAll()
 
     @Delete
-    suspend fun delete(news: News)
+    fun delete(news: News)
 }
